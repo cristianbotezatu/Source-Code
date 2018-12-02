8ee5adc19df8bcc7004c0f1009f7bdc3cbd600c8
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 01:53:51 +0600

    Added comments to describe each statement

--- Week 3/task1.py
+++ Week 3/task1.py
@@ -1,7 +1,11 @@
+#this line asks for a number in a new line showing "Input your first number" 
 firstNumber = int(input("Input your first number\n"))
 
+#this line asks for a number in a new line showing "Input your second number" 
 secondNumber = int(input("Input your second number\n"))
 
+#this line stores the sum of firstNumber and secondNumber variables in summationResult
 summationResult = firstNumber + secondNumber
 
+#this line prints the two inputted number along with the summation of these two numbers
 print("The sum of {0} and {1} is {2}".format(firstNumber, secondNumber, summationResult))

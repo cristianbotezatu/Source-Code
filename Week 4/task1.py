e956207be1ff3805d0a43751e8ed4845a0caeed7
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 02:11:39 +0600

    Added comments to describe each statement

--- Week 4/task1.py
+++ Week 4/task1.py
@@ -1,4 +1,12 @@
-radius = input("Radius:")
+#this line was wrong as input takes string but this variable is integer
+#Current statement saves the input as int
+radius = int(input("Radius:"))
+
+#this statement saves 3.14 value in x variable
 x = 3.14
+
+#this statement saves the value of variable x in variable pi
 pi = x
+
+#this statement saves the area using the x and radius variables
 area = pi * radius ** 2

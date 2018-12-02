01eb3ff8c256f9d4ea9379c8333f07914595e00e
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 01:00:09 +0600

    Create task1.py

---
+++ Week 3/task1.py
@@ -0,0 +1,7 @@
+firstNumber = int(input("Input your first number\n"))
+
+secondNumber = int(input("Input your second number\n"))
+
+summationResult = firstNumber + secondNumber
+
+print("The sum of {0} and {1} is {2}".format(firstNumber, secondNumber, summationResult))

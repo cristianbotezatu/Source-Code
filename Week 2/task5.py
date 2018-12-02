143f2c0e3081fc151e80c81d63c16961ffd32a50
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 01:41:22 +0600

    Added comments to describe each statement

--- Week 2/task5.py
+++ Week 2/task5.py
@@ -1,3 +1,8 @@
+#this line prints "Hi there, what is your name?" and asks to input name in a new line
 username = input("Hi there, what is your name?\n")
 
+#this line prints 
+#Hello
+#name (inputted by the user)
+#How are you?
 print("Hello\n\""+username+"\"\nHow are you?")

edcad339be3aafd021512bdf4e7963afbcd4ac42
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 03:25:29 +0600

    Wrote statements prompting and printing name & age
    Useful comments have been added

--- Week 4/task8.py
+++ Week 4/task8.py
@@ -1,1 +1,8 @@
-Hello ____, next year you will be ____ years old!
+#taking username as input prompting for their username
+name = input("Enter your name : ")
+
+#taking age as input prompting for their age
+age = int(input("Enter your age : "))
+
+#printing the inputted name and age provided by the user inside a string
+print("Hello {0}, next year you will be {1} years old!".format(name,age))

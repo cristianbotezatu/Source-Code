010ee0ac30c6c497e8bf2eeb674d944ea72304fc
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 03:14:16 +0600

    Corrected run-time errors adding useful comments

--- Week 4/task7.py
+++ Week 4/task7.py
@@ -1,5 +1,14 @@
+#to call sqrt function
 from math import sqrt
-X = 2
-Y = 4
-print(“The product of “, x, “and”, y, “is”, x + y)
-print(“The root of their difference is “, sqrt(x – y))
+
+#storing 2 in variable x
+x = 2
+
+#storing 4 in variable y
+y = 4
+
+#printing product of x and y
+print("The product of ", x, "and", y, "is", x * y)
+
+#printing the root of the difference of x and y
+print("The root of their difference is ", sqrt(y - x))

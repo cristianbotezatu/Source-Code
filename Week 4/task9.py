d34a5f7bff3243e84b500d32f8e309184d5fe772
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 04:30:50 +0600

    Updated code and printed to two decimal points

--- Week 4/task9.py
+++ Week 4/task9.py
@@ -1,1 +1,8 @@
+#storing 2 in the variable radius
+radius = 2
+
+#storing 12.5678 in the variable area
+area = 12.5678
+
+#printing radius to two decimal points and area 
 print("Radius is: {}\nArea is: {:0.2f}".format(radius,area))

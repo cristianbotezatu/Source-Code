192bb8fba221b47937275761c4782070a077f8d9
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 03:04:33 +0600

    Corrected the compile-time errors adding  comments

--- Week 4/task6.py
+++ Week 4/task6.py
@@ -1,3 +1,13 @@
-int x = 2
-Print (x, squared is, x * x)
-xcubed = x *** 3
+#declaring adding data type is not necessary in python
+#therefore, int has been removed
+x = 2
+
+#the first letter should be small letter p
+#the string "squared is" should be closed in double quatation (")
+print (x, "squared is", x * x)
+
+#to show x cubed two (*) is needed not three
+xcubed = x ** 3
+
+#this statement prints xcubed
+print(x, "cubed is", xcubed)

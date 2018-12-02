72428ae03235047d643024af8e4b9b4f096cf1ca
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 02:53:01 +0600

    Printing to two decimal places and added comments

--- Week 4/task5.py
+++ Week 4/task5.py
@@ -1,4 +1,11 @@
+#this statement stores 19.93 in the x variable
 x = 19.93
+
+#this statement stores 20.00 in the y variable
 y = 20.00
-z = y – x
-print(z)
+
+#this statement stores substraction of y from x in the z variable
+z = y - x
+
+#this statement prints the variable z to two decimal places
+print("%.2f"%z)

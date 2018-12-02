7e9744064a9fd5c233d56a59d69ee86c15b217c4
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 02:14:33 +0600

    Minor update

--- Week 4/task1.py
+++ Week 4/task1.py
@@ -1,6 +1,6 @@
 #this line was wrong as input takes string but this variable is integer
 #Current statement saves the input as int
-radius = int(input("Radius:"))
+radius = int(input("Radius: "))
 
 #this statement saves 3.14 value in x variable
 x = 3.14

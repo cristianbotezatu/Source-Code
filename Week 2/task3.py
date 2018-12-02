fce5b5f1b77d86f171fdb912f5037c0b63210b3e
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-12-03 01:33:00 +0600

    Added comments to describe each statement

--- Week 2/task3.py
+++ Week 2/task3.py
@@ -1,3 +1,8 @@
+#this line stores "Hello World" string in firstSentence variable 
 firstSentence = "Hello World"
+
+#this line stores "I am in my ISD class right now" string in secondSentence variable
 secondSentence = "I am in my ISD class right now"
+
+#this line concatenates the variables and prints them
 print(firstSentence+"\n"+secondSentence)
